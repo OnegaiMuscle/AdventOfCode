0fe645cc1f0b2cb85480b07620fea3f4100b4e06
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-22 14:47:28 +0100

    AOC

--- 2015/answerDay23.js
+++ 2015/answerDay23.js
@@ -1,71 +1,50 @@
+let a = 0;
+let b = 0;
+let i = -1;
+let inst = [];
 const fs = require('fs');
 
-function parseInstructions(filePath) {
-    return fs.readFileSync(filePath, 'utf8')
-        .split('\n')
-        .map(line => line.trim())
-        .filter(line => line.length > 0);
-}
+fs.readFileSync('inputDay23.txt', 'utf-8').split('\n').forEach(instr => {
+    inst.push(instr.replace(',', '').split(' '));
+});
 
-function runProgram(instructions) {
-    let registers = { a: 0, b: 0 };
-    let pointer = 0;
-    const memo = {};
-
-    function execute(pointer, registers) {
-        const key = `${pointer}-${registers.a}-${registers.b}`;
-        if (memo[key] !== undefined) return memo[key];
-
-        while (pointer >= 0 && pointer < instructions.length) {
-            let [instruction, arg1, arg2] = instructions[pointer].split(' ');
-
-            switch (instruction) {
-                case 'hlf':
-                    registers[arg1] = Math.floor(registers[arg1] / 2);
-                    pointer++;
-                    break;
-                case 'tpl':
-                    registers[arg1] *= 3;
-                    pointer++;
-                    break;
-                case 'inc':
-                    registers[arg1]++;
-                    pointer++;
-                    break;
-                case 'jmp':
-                    pointer += parseInt(arg1);
-                    break;
-                case 'jie':
-                    if (registers[arg1] % 2 === 0) {
-                        pointer += parseInt(arg2);
-                    } else {
-                        pointer++;
-                    }
-                    break;
-                case 'jio':
-                    if (registers[arg1] === 1) {
-                        pointer += parseInt(arg2);
-                    } else {
-                        pointer++;
-                    }
-                    break;
-                default:
-                    throw new Error(`Unknown instruction: ${instruction}`);
+while (++i < inst.length) {
+    switch (inst[i][0]) {
+        case 'hlf':
+            if (inst[i][1] === 'a') {
+                a /= 2;
+            } else {
+                b /= 2;
             }
-
-            const result = execute(pointer, { ...registers });
-            memo[key] = result;
-            return result;
-        }
-
-        return registers.b;
+            break;
+        case 'tpl':
+            if (inst[i][1] === 'a') {
+                a *= 3;
+            } else {
+                b *= 3;
+            }
+            break;
+        case 'inc':
+            if (inst[i][1] === 'a') {
+                a += 1;
+            } else {
+                b += 1;
+            }
+            break;
+        case 'jmp':
+            i += parseInt(inst[i][1]) - 1;
+            break;
+        case 'jie':
+            if ((inst[i][1] === 'a' ? a : b) % 2 === 0) {
+                i += parseInt(inst[i][2]) - 1;
+            }
+            break;
+        case 'jio':
+            if ((inst[i][1] === 'a' ? a : b) === 1) {
+                i += parseInt(inst[i][2]) - 1;
+            }
+            break;
     }
-
-    return execute(pointer, registers);
 }
 
-// Example usage:
-const filePath = 'inputDay23.txt';
-const instructions = parseInstructions(filePath);
-const result = runProgram(instructions);
-console.log(`The value in register b is: ${result}`);
+console.log(b);
