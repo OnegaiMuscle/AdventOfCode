e560b2787a165eda42075943a973d2d9c9a46e08
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-24 21:10:17 +0100

    AOC

--- 2016/answerDay02.js
+++ 2016/answerDay02.js
@@ -37,6 +37,47 @@ function getBathroomCode(instructions) {
     return code;
 }
 
+function getBathroomCodebis(instructions) {
+    const keypad = [
+        [null, null, '1', null, null],
+        [null, '2', '3', '4', null],
+        ['5', '6', '7', '8', '9'],
+        [null, 'A', 'B', 'C', null],
+        [null, null, 'D', null, null]
+    ];
+    let x = 2, y = 0; // Start at '5'
+    let code = '';
+
+    instructions.forEach(line => {
+        for (const move of line) {
+            let newX = x, newY = y;
+            switch (move) {
+                case 'U':
+                    newX = x > 0 ? x - 1 : x;
+                    break;
+                case 'D':
+                    newX = x < 4 ? x + 1 : x;
+                    break;
+                case 'L':
+                    newY = y > 0 ? y - 1 : y;
+                    break;
+                case 'R':
+                    newY = y < 4 ? y + 1 : y;
+                    break;
+            }
+            if (keypad[newX][newY] !== null) {
+                x = newX;
+                y = newY;
+            }
+        }
+        code += keypad[x][y];
+    });
+
+    return code;
+}
+
 const instructions = parseData('inputDay02.txt');
 const bathroomCode = getBathroomCode(instructions);
 console.log(`Bathroom code: ${bathroomCode}`);
+const bathroomCodebis = getBathroomCodebis(instructions);
+console.log(`Bathroom code: ${bathroomCodebis}`);
