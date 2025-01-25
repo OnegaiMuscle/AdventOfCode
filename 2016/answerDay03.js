1592bdabb33f76b87b2e50e8fd0ae09909428087
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-25 20:54:55 +0100

    AOC

--- 2016/answerDay03.js
+++ 2016/answerDay03.js
@@ -14,7 +14,25 @@ function countValidTriangles(triangles) {
     return triangles.filter(isValidTriangle).length;
 }
 
-// Exemple d'utilisation
 const triangles = parseData('inputDay03.txt');
 const validTrianglesCount = countValidTriangles(triangles);
 console.log(`Number of valid triangles: ${validTrianglesCount}`);
+
+
+function parseDatabis(filePath) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.trim().split('\n').map(line => line.trim().split(/\s+/).map(Number));
+    const triangles = [];
+
+    for (let i = 0; i < lines.length; i += 3) {
+        for (let j = 0; j < 3; j++) {
+            triangles.push([lines[i][j], lines[i + 1][j], lines[i + 2][j]]);
+        }
+    }
+
+    return triangles;
+}
+
+const trianglesbis = parseDatabis('inputDay03.txt');
+const validTrianglesCountbis = countValidTriangles(trianglesbis);
+console.log(`Number of valid triangles: ${validTrianglesCountbis}`);
