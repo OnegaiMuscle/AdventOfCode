f4d84d082cd3a85faea1c127af5c8b44f5b016b1
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-27 23:51:13 +0100

    AOC

---
+++ 2016/answerDay06.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function parseData(filePath) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return data.trim().split('\n');
+}
+
+function getErrorCorrectedMessage(messages) {
+    const messageLength = messages[0].length;
+    const frequency = Array.from({ length: messageLength }, () => ({}));
+
+    messages.forEach(message => {
+        for (let i = 0; i < messageLength; i++) {
+            const char = message[i];
+            if (!frequency[i][char]) {
+                frequency[i][char] = 0;
+            }
+            frequency[i][char]++;
+        }
+    });
+
+    return frequency.map(freq => {
+        return Object.keys(freq).reduce((a, b) => freq[a] > freq[b] ? a : b);
+    }).join('');
+}
+
+// Exemple d'utilisation
+const messages = parseData('inputDay06.txt');
+const errorCorrectedMessage = getErrorCorrectedMessage(messages);
+console.log(`Error-corrected message: ${errorCorrectedMessage}`);
