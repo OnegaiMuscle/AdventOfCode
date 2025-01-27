4fbfcf5ea4e00b6214972d72bd039a9aaddc574e
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-27 23:47:50 +0100

    AOC

--- 2016/answerDay05.js
+++ 2016/answerDay05.js
@@ -18,3 +18,28 @@ function findPassword(doorID) {
 const doorID = 'ffykfhsq';
 const password = findPassword(doorID);
 console.log(`Password: ${password}`);
+
+
+function findPasswordbis(doorID) {
+    let password = Array(8).fill(null);
+    let index = 0;
+    let found = 0;
+
+    while (found < 8) {
+        const hash = crypto.createHash('md5').update(doorID + index).digest('hex');
+        if (hash.startsWith('00000')) {
+            const position = parseInt(hash[5], 10);
+            if (position >= 0 && position < 8 && password[position] === null) {
+                password[position] = hash[6];
+                found++;
+            }
+        }
+        index++;
+    }
+
+    return password.join('');
+}
+
+
+const passwordbis = findPasswordbis(doorID);
+console.log(`Password: ${passwordbis}`);
