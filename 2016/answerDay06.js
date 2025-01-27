f8c099bf58a73ee5a63c156fb4ebab9d9354adc9
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-27 23:52:55 +0100

    AOC

--- 2016/answerDay06.js
+++ 2016/answerDay06.js
@@ -24,7 +24,29 @@ function getErrorCorrectedMessage(messages) {
     }).join('');
 }
 
-// Exemple d'utilisation
 const messages = parseData('inputDay06.txt');
 const errorCorrectedMessage = getErrorCorrectedMessage(messages);
 console.log(`Error-corrected message: ${errorCorrectedMessage}`);
+
+
+function getOriginalMessage(messages) {
+    const messageLength = messages[0].length;
+    const frequency = Array.from({ length: messageLength }, () => ({}));
+
+    messages.forEach(message => {
+        for (let i = 0; i < messageLength; i++) {
+            const char = message[i];
+            if (!frequency[i][char]) {
+                frequency[i][char] = 0;
+            }
+            frequency[i][char]++;
+        }
+    });
+
+    return frequency.map(freq => {
+        return Object.keys(freq).reduce((a, b) => freq[a] < freq[b] ? a : b);
+    }).join('');
+}
+
+const originalMessage = getOriginalMessage(messages);
+console.log(`Original message: ${originalMessage}`);
