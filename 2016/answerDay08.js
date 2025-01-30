c765a70bfac87e747788dbc042484d1c9412c141
Duc Tai TRAN <tdts2010@gmail.com>
2025-01-30 23:58:29 +0100

    AOC

--- 2016/answerDay08.js
+++ 2016/answerDay08.js
@@ -55,3 +55,48 @@ instructions.forEach(instruction => applyInstruction(screen, instruction));
 
 const litPixels = countLitPixels(screen);
 console.log(`Number of lit pixels: ${litPixels}`);
+
+
+
+
+function applyInstructionbis(screen, instruction) {
+    const rectMatch = instruction.match(/rect (\d+)x(\d+)/);
+    const rotateRowMatch = instruction.match(/rotate row y=(\d+) by (\d+)/);
+    const rotateColumnMatch = instruction.match(/rotate column x=(\d+) by (\d+)/);
+
+    if (rectMatch) {
+        const [_, width, height] = rectMatch.map(Number);
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                screen[y][x] = true;
+            }
+        }
+    } else if (rotateRowMatch) {
+        const [_, row, shift] = rotateRowMatch.map(Number);
+        const newRow = Array(screen[0].length);
+        for (let x = 0; x < screen[0].length; x++) {
+            newRow[(x + shift) % screen[0].length] = screen[row][x];
+        }
+        screen[row] = newRow;
+    } else if (rotateColumnMatch) {
+        const [_, col, shift] = rotateColumnMatch.map(Number);
+        const newCol = Array(screen.length);
+        for (let y = 0; y < screen.length; y++) {
+            newCol[(y + shift) % screen.length] = screen[y][col];
+        }
+        for (let y = 0; y < screen.length; y++) {
+            screen[y][col] = newCol[y];
+        }
+    }
+}
+
+function displayScreenbis(screen) {
+    return screen.map(row => row.map(pixel => (pixel ? '#' : '.')).join('')).join('\n');
+}
+
+
+
+
+instructions.forEach(instruction => applyInstructionbis(screen, instruction));
+
+console.log(displayScreenbis(screen));
