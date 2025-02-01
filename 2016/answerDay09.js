ec79e536197459201bb9ed9d1e9befa9b034c5b2
Duc Tai TRAN <tdts2010@gmail.com>
2025-02-01 22:23:10 +0100

    AOC

--- 2016/answerDay09.js
+++ 2016/answerDay09.js
@@ -25,7 +25,34 @@ function decompressLength(data) {
     return length;
 }
 
-// Exemple d'utilisation
 const data = parseData('inputDay09.txt');
 const decompressedLength = decompressLength(data);
 console.log(`Decompressed length: ${decompressedLength}`);
+
+
+function decompressLengthV2(data) {
+    function calculateLength(data, start, end) {
+        let length = 0;
+        let i = start;
+
+        while (i < end) {
+            if (data[i] === '(') {
+                const markerEnd = data.indexOf(')', i);
+                const marker = data.slice(i + 1, markerEnd).split('x').map(Number);
+                const [chars, repeat] = marker;
+                length += repeat * calculateLength(data, markerEnd + 1, markerEnd + 1 + chars);
+                i = markerEnd + 1 + chars;
+            } else {
+                length++;
+                i++;
+            }
+        }
+
+        return length;
+    }
+
+    return calculateLength(data, 0, data.length);
+}
+
+const decompressedLengthV2 = decompressLengthV2(data);
+console.log(`Decompressed length (version 2): ${decompressedLengthV2}`);
