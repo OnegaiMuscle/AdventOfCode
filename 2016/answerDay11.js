4f5a0f1854ea8d06d90af57117ae8fa25a55b37e
Duc Tai TRAN <tdts2010@gmail.com>
2025-02-03 23:07:17 +0100

    AOC

--- 2016/answerDay11.js
+++ 2016/answerDay11.js
@@ -1,102 +1,24 @@
-const fs = require('fs');
+const itemsPartOne = [4, 2, 4, 0];
+const itemsPartTwo = [8, 2, 4, 0];
 
-function parseData(filePath) {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    return data.trim().split('\n');
-}
-
-function isValidState(state) {
-    for (let floor of state) {
-        let generators = new Set();
-        let microchips = new Set();
-        for (let item of floor) {
-            if (item.endsWith('G')) {
-                generators.add(item[0]);
-            } else {
-                microchips.add(item[0]);
-            }
+function getMoves(items) {
+    
+    let moves = 0;
+    while (items[items.length - 1] !== items.reduce((a, b) => a + b, 0)) {
+        // console.log(moves, items);
+        let lowestFloor = 0;
+        while (items[lowestFloor] === 0) {
+            lowestFloor++;
         }
-        if (microchips.size > 0 && generators.size > 0) {
-            for (let chip of microchips) {
-                if (!generators.has(chip)) {
-                    return false;
-                }
-            }
-        }
+        moves += 2 * (items[lowestFloor] - 1) - 1;
+        items[lowestFloor + 1] += items[lowestFloor];
+        items[lowestFloor] = 0;
     }
-    return true;
+    return moves;
 }
 
-function getNextStates(state, elevator) {
-    let nextStates = [];
-    let currentFloor = state[elevator];
-    let items = [...currentFloor];
-    for (let i = 0; i < items.length; i++) {
-        for (let j = i; j < items.length; j++) {
-            let newState = state.map(floor => new Set(floor));
-            newState[elevator].delete(items[i]);
-            if (i !== j) {
-                newState[elevator].delete(items[j]);
-            }
-            if (elevator > 0) {
-                newState[elevator - 1].add(items[i]);
-                if (i !== j) {
-                    newState[elevator - 1].add(items[j]);
-                }
-                if (isValidState(newState)) {
-                    nextStates.push([newState, elevator - 1]);
-                }
-                newState[elevator - 1].delete(items[i]);
-                if (i !== j) {
-                    newState[elevator - 1].delete(items[j]);
-                }
-            }
-            if (elevator < 3) {
-                newState[elevator + 1].add(items[i]);
-                if (i !== j) {
-                    newState[elevator + 1].add(items[j]);
-                }
-                if (isValidState(newState)) {
-                    nextStates.push([newState, elevator + 1]);
-                }
-            }
-        }
-    }
-    return nextStates;
-}
-
-function serializeState(state, elevator) {
-    return JSON.stringify(state.map(floor => [...floor].sort())) + elevator;
-}
-
-function minStepsToAssemble(state, elevator) {
-    let queue = [[state, elevator, 0]];
-    let visited = new Set();
-    visited.add(serializeState(state, elevator));
-    while (queue.length > 0) {
-        let [currentState, currentElevator, steps] = queue.shift();
-        if (currentState[3].size === 10) {
-            return steps;
-        }
-        for (let [nextState, nextElevator] of getNextStates(currentState, currentElevator)) {
-            let serializedState = serializeState(nextState, nextElevator);
-            if (!visited.has(serializedState)) {
-                visited.add(serializedState);
-                queue.push([nextState, nextElevator, steps + 1]);
-            }
-        }
-    }
-    return -1;
-}
-
-// Exemple d'utilisation
-const data = parseData('inputDay11.txt');
-const initialState = [
-    new Set(['PG', 'TG', 'TM', 'RG', 'CG', 'RM', 'PM', 'CM']),
-    new Set(['PM', 'CM']),
-    new Set(),
-    new Set()
-];
-const initialElevator = 0;
-const steps = minStepsToAssemble(initialState, initialElevator);
-console.log(`Minimum number of steps: ${steps}`);
+console.log("Part One");
+console.log(getMoves(itemsPartOne));
+console.log("");
+console.log("Part Two");
+console.log(getMoves(itemsPartTwo));
