be47c9b2bbf44d98605c0ba9250717e7e73ca10e
Duc Tai TRAN <tdts2010@gmail.com>
2025-02-11 23:12:49 +0100

    AOC

--- 2016/answerDay19.js
+++ 2016/answerDay19.js
@@ -9,3 +9,27 @@ function josephus(numberOfElves) {
 const numberOfElves = 3014387;
 const winningElf = josephus(numberOfElves);
 console.log(`The Elf that gets all the presents is: ${winningElf}`);
+
+function findWinningElf(numberOfElves) {
+  let power = 1;
+  while (power * 3 < numberOfElves) {
+      power *= 3;
+  }
+
+  const remaining = numberOfElves - power;
+  if (remaining === 0) {
+      return numberOfElves;
+  }
+
+  if (remaining <= power) {
+      return remaining;
+  } else {
+      return power + remaining;
+  }
+}
+
+
+
+
+const winningElfbis = findWinningElf(numberOfElves);
+console.log(`The Elf that gets all the presents is: ${winningElfbis}`);
