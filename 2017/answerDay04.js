d2b7055c5ee4e303a614825dfa5d216a7b350c00
TRAN Duc Tai <tdts2010@gmail.com>
2025-03-14 20:52:58 +0100

    AOC

--- 2017/answerDay04.js
+++ 2017/answerDay04.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
 
+function countValidPassphrases(filePath) {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const passphrases = data.split('\n').map(line => line.trim());
+
+  const validCount = passphrases
+    .map(passphrase => passphrase.split(' '))
+    .filter(words => new Set(words).size === words.length).length;
+
+  return validCount;
+}
+
+// Example usage:
+const filePath = 'inputDay04.txt'; 
+const validPassphrases = countValidPassphrases(filePath);
+
+console.log("Number of Valid Passphrases:", validPassphrases);
