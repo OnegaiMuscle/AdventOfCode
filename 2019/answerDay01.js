cf5c993cdb96a47b7efd91045aa8062503fba8d6
TRAN Duc Tai <tdts2010@gmail.com>
2025-04-04 17:58:05 +0200

    AOC

--- 2019/answerDay01.js
+++ 2019/answerDay01.js
@@ -4,12 +4,26 @@ function calculateFuel(mass) {
     return Math.floor(mass / 3) - 2;
 }
 
-function getTotalFuelRequirement(filename) {
+function calculateTotalFuel(mass) {
+    let totalFuel = 0;
+    while (mass > 0) {
+        mass = calculateFuel(mass);
+        if (mass > 0) totalFuel += mass;
+    }
+    return totalFuel;
+}
+
+function getFuelRequirements(filename) {
     const data = fs.readFileSync(filename, 'utf8');
     const masses = data.trim().split('\n').map(Number);
-    return masses.reduce((totalFuel, mass) => totalFuel + calculateFuel(mass), 0);
+
+    const basicFuel = masses.reduce((totalFuel, mass) => totalFuel + calculateFuel(mass), 0);
+    const totalFuel = masses.reduce((totalFuel, mass) => totalFuel + calculateTotalFuel(mass), 0);
+
+    return { basicFuel, totalFuel };
 }
 
 // Example usage
-const totalFuel = getTotalFuelRequirement('inputDay01.txt');
-console.log('Total fuel requirement:', totalFuel);
+const fuelRequirements = getFuelRequirements('inputDay01.txt');
+console.log('Basic fuel requirement:', fuelRequirements.basicFuel);
+console.log('Total fuel requirement (including fuel for fuel):', fuelRequirements.totalFuel);
