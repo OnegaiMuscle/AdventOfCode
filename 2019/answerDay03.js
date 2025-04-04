9527f96b716413b6e4a5b169ec63053def9586d2
TRAN Duc Tai <tdts2010@gmail.com>
2025-04-04 18:10:46 +0200

    AOC

--- 2019/answerDay03.js
+++ 2019/answerDay03.js
@@ -1,25 +1,32 @@
 const fs = require('fs');
 
+
+
+// Wire Path Functions
 function getWirePaths(filename) {
     const data = fs.readFileSync(filename, 'utf8');
     return data.trim().split('\n').map(line => line.split(','));
 }
 
 function traceWirePath(instructions) {
-    const path = new Set();
-    let x = 0, y = 0;
+    const path = new Map();
+    let x = 0, y = 0, steps = 0;
 
     for (const instruction of instructions) {
         const direction = instruction[0];
-        const steps = parseInt(instruction.slice(1));
+        const distance = parseInt(instruction.slice(1));
 
-        for (let i = 0; i < steps; i++) {
+        for (let i = 0; i < distance; i++) {
             if (direction === 'R') x++;
             if (direction === 'L') x--;
             if (direction === 'U') y++;
             if (direction === 'D') y--;
 
-            path.add(`${x},${y}`);
+            steps++;
+            const key = `${x},${y}`;
+            if (!path.has(key)) {
+                path.set(key, steps);
+            }
         }
     }
 
@@ -32,7 +39,7 @@ function findClosestIntersection(filename) {
     const path1 = traceWirePath(wire1);
     const path2 = traceWirePath(wire2);
 
-    const intersections = [...path1].filter(point => path2.has(point));
+    const intersections = [...path1.keys()].filter(point => path2.has(point));
 
     const distances = intersections.map(point => {
         const [x, y] = point.split(',').map(Number);
@@ -42,6 +49,23 @@ function findClosestIntersection(filename) {
     return Math.min(...distances);
 }
 
-// Example usage
-const closestDistance = findClosestIntersection('inputDay03.txt');
-console.log('Closest intersection distance:', closestDistance);
+function findFewestStepsIntersection(filename) {
+    const [wire1, wire2] = getWirePaths(filename);
+
+    const path1 = traceWirePath(wire1);
+    const path2 = traceWirePath(wire2);
+
+    const intersections = [...path1.keys()].filter(point => path2.has(point));
+
+    const fewestSteps = intersections.map(point => path1.get(point) + path2.get(point));
+
+    return Math.min(...fewestSteps);
+}
+
+
+
+const closestIntersectionDistance = findClosestIntersection('inputDay03.txt');
+console.log('Closest intersection distance:', closestIntersectionDistance);
+
+const fewestStepsIntersection = findFewestStepsIntersection('inputDay03.txt');
+console.log('Fewest combined steps to intersection:', fewestStepsIntersection);
